f2dede3bdb0f2186ca2eb84bdb0c2e57228477f0
MR.Parth mahadik <parthmahadik44@gmail.com>
2026-01-12 10:15:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,4 +146,5 @@ def predict():
 # ---------------------------
 if __name__ == '__main__':
     load_trained_model()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
